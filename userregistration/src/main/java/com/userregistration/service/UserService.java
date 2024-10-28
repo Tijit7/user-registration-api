62391dbba596b1da213b73b0b91c38c37a53366c
Tijit7 <64460757+Tijit7@users.noreply.github.com>
2024-10-28 22:31:07 +0530

    user details

--- userregistration/src/main/java/com/userregistration/service/UserService.java
+++ userregistration/src/main/java/com/userregistration/service/UserService.java
@@ -1,5 +1,9 @@
-package com.userregistration;
+package com.userregistration.service;
 
+import com.userregistration.model.User;
+import com.userregistration.model.MongoUser;
+import com.userregistration.repository.UserRepository;
+import com.userregistration.repository.MongoUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,12 +16,15 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private MongoUserRepository mongoUserRepository;
+
    
     public User registerUser(User user) {
         return userRepository.save(user);
     }
 
-  
+   
     public User updateUser(Long id, User userDetails) {
         Optional<User> userOpt = userRepository.findById(id);
         if (userOpt.isPresent()) {
@@ -27,10 +34,10 @@ public class UserService {
             user.setPassword(userDetails.getPassword());
             return userRepository.save(user);
         }
-        return null; 
+        return null; // or throw an exception
     }
 
-    
+  
     public void deleteUser(Long id) {
         userRepository.deleteById(id);
     }
@@ -40,8 +47,41 @@ public class UserService {
         return userRepository.findAll();
     }
 
-    
+  
     public User getUserByEmail(String email) {
         return userRepository.findByEmail(email);
+    }
+
+  
+    public MongoUser registerMongoUser(MongoUser mongoUser) {
+        return mongoUserRepository.save(mongoUser);
+    }
+
+   
+    public MongoUser updateMongoUser(String id, MongoUser userDetails) {
+        Optional<MongoUser> userOpt = mongoUserRepository.findById(id);
+        if (userOpt.isPresent()) {
+            MongoUser user = userOpt.get();
+            user.setName(userDetails.getName());
+            user.setGender(userDetails.getGender());
+            user.setPassword(userDetails.getPassword());
+            return mongoUserRepository.save(user);
+        }
+        return null;
+    }
+
+   
+    public void deleteMongoUser(String id) {
+        mongoUserRepository.deleteById(id);
+    }
+
+   
+    public List<MongoUser> getAllMongoUsers() {
+        return mongoUserRepository.findAll();
+    }
+
+    
+    public MongoUser getMongoUserByEmail(String email) {
+        return mongoUserRepository.findByEmail(email);
     }
 }
