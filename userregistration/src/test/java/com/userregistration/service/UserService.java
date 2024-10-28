8e05e161fb462eeb06ae4a28b64e4435f1405cdf
Tijit7 <64460757+Tijit7@users.noreply.github.com>
2024-10-28 22:06:32 +0530

    Add files via upload

---
+++ userregistration/src/test/java/com/userregistration/service/UserService.java
@@ -0,0 +1,12 @@
+package com.userregistration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    // Other service methods (register, login, etc.)
+}
