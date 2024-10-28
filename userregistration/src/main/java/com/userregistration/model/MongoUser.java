fd8bfecce8decbd159eb19a1a2c19932462ea4a6
Tijit7 <64460757+Tijit7@users.noreply.github.com>
2024-10-28 22:25:28 +0530

    mogo db entity

--- userregistration/src/main/java/com/userregistration/model/MongoUser.java
+++ userregistration/src/main/java/com/userregistration/model/MongoUser.java
@@ -1,4 +1,4 @@
-package com.userregistration;
+package com.userregistration.model;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -11,6 +11,46 @@ public class MongoUser {
     private String email;
     private String name;
     private String gender;
+    private String password;
 
-   
+    // Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
